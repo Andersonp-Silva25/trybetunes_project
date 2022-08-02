3378598f9d2c4342b042bffcbe524947dd814ad0
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2022-08-02 17:53:08 -0300

    Requisito 5

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,11 +2,45 @@ import React, { Component } from 'react';
 import Header from './Header';
 
 export default class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isDisable: true,
+    };
+  }
+
+  verifyInput = ({ target: { value } }) => {
+    const lengthInput = value.length >= 2;
+    this.setState({ isDisable: !lengthInput });
+  }
+
   render() {
+    const { isDisable } = this.state;
     return (
       <>
         <Header />
-        <div data-testid="page-search">Search</div>
+        <div data-testid="page-search">
+          <form>
+            <label htmlFor="search">
+              <input
+                type="text"
+                name="search"
+                id="search"
+                placeholder="Nome do Artista ou Banda"
+                data-testid="search-artist-input"
+                onChange={ this.verifyInput }
+              />
+            </label>
+            <button
+              type="submit"
+              data-testid="search-artist-button"
+              disabled={ isDisable }
+            >
+              Pesquisar
+
+            </button>
+          </form>
+        </div>
       </>
     );
   }
